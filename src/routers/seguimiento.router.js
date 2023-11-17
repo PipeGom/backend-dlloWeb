0fd71317c4fc4e32d57cdf5310aac1bb2424ce6a
juan <juancm1106.2@gmail.com>
2023-11-17 00:16:31 -0500

    Fix: seguimiento

--- src/routers/seguimiento.router.js
+++ src/routers/seguimiento.router.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const SeguimientoController = require('../controllers/seguimiento.controller');
+const express = require("express")
 
-const SeguimientoController = new SeguimientoController();
+const router = express.Router()
+const { SeguimientoController} = require("../controllers");
+const _seguimientocontroller = new SeguimientoController()
 
-router.get("/", seguimientoController.getSeguimientos)
-router.get("/:id", seguimientoController.getSeguimiento)
-router.post('/', seguimientoController.createSeguimiento);
-router.delete('/:id', seguimientoController.deleteSeguimiento);
-router.put('/:id', seguimientoController.updateSeguimiento);
 
-module.exports = router;
+router.get("/", _seguimientocontroller.getSeguimientos)
+router.get("/:id", _seguimientocontroller.getSeguimiento)
+router.post("/", _seguimientocontroller.createSeguimiento) //parametros de ruta
+router.put("/:id",_seguimientocontroller.updateSeguimiento)
+router.delete("/id", _seguimientocontroller.deleteSeguimiento)
+
+module.exports = router
